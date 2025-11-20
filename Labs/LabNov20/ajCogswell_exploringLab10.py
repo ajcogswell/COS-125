7a5ba935843ad5089043dbdbe548a43b6a22f4c5
ajcogswell <ajcogswell797@icloud.com>
2025-11-20 13:20:59 -0500

    lab

--- Labs/LabNov20/ajCogswell_exploringLab10.py
+++ Labs/LabNov20/ajCogswell_exploringLab10.py
@@ -3,3 +3,26 @@ print(namesFile)
 
 for name in namesFile:
     print(name)
+
+namesFile.close()
+
+namesFile = open('names.txt', 'a')
+addName = True
+nameList = []
+
+while addName:
+    userInput = input("Enter a name:")
+    nameList.append(userInput)
+    replay = input("Do you want to add another name?").lower()
+    if replay == "no" or replay == "n":
+        addName = False
+    elif replay == "yes" or replay == "y":
+        addName = True
+    else:
+        print("Something went wrong...")
+
+for i in nameList:
+    namesFile.write(i)
+
+namesFile.close()
+
